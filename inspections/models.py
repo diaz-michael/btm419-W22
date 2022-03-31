a1fffe77c8245dd7f14df1e426e179d8eeb293ba
Michael D <michael@diaz.info>
2022-03-31 12:15:11 -0600

    vin validator

--- inspections/models.py
+++ inspections/models.py
@@ -1,6 +1,46 @@
 from bdb import effective
 from django.db import models
 from django.urls import reverse
+from django.core.exceptions import ValidationError
+
+def validate_vin(value):
+    POSITIONAL_WEIGHTS = [8,7,6,5,4,3,2,10,0,9,8,7,6,5,4,3,2]
+    ILLEGAL_ALL = ['I', 'O', 'Q']
+    ILLEGAL_TENTH = ['U','Z','0']
+    LETTER_KEY = dict(
+        A=1,B=2,C=3,D=4,E=5,F=6,G=7,H=8,
+        J=1,K=2,L=3,M=4,N=5,    P=7,    R=9,
+            S=2,T=3,U=4,V=5,W=6,X=7,Y=8,Z=9,
+    )
+
+    if len(value) == 17:
+        vin = value.upper()
+
+        for char in ILLEGAL_ALL:
+            if char in vin:
+                raise ValidationError('Field cannot contain "I", "O", or "Q".')
+
+        if vin[9] in ILLEGAL_TENTH:
+            raise ValidationError('Field cannot contain "U", "Z", or "0" in position 10.')
+
+        check_digit = vin[8]
+
+        pos=sum=0
+        for char in vin:
+            value = int(LETTER_KEY[char]) if char in LETTER_KEY else int(char)
+            weight = POSITIONAL_WEIGHTS[pos]
+            sum += (value * weight)
+            pos += 1
+
+        calc_check_digit = int(sum) % 11
+
+        if calc_check_digit == 10:
+            calc_check_digit = 'X'
+
+        if str(check_digit) != str(calc_check_digit):
+            raise ValidationError('Invalid VIN.')
+    else:
+        raise ValidationError('Field must be 17 characters.')
 
 # Create your models here.
 class warrantyManager(models.Manager):
@@ -54,7 +94,9 @@ class inspectionManager(models.Manager):
 
 class inspection(models.Model):
     claimID = models.ForeignKey(claim, on_delete=models.PROTECT)
-    vin = models.CharField(max_length=200)
+    vin = models.CharField(
+        max_length=200,
+        validators =[validate_vin])
     make = models.CharField(max_length=200)
     model = models.CharField(max_length=200)
     year = models.DecimalField(max_digits=4, decimal_places=0)
