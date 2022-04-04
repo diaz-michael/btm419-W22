c02adcd8c277a43fabffe1f824d970b12387baf3
Michael D <michael@diaz.info>
2022-04-03 21:43:42 -0600

    Reporting Fixs

--- reporting/views.py
+++ reporting/views.py
@@ -1,6 +1,7 @@
-from django.db.models import Count, Sum, F, FloatField, Avg, Max, Min
+from django.db.models import Count, Sum, F, FloatField, Avg, Max, Min, CharField
 
-from django.db.models import Q
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from django.shortcuts import render
 from inventory.models import order_form
 from inspections.models import inspection
@@ -8,7 +9,6 @@ from background.models import Product
 from io import BytesIO
 import matplotlib.pyplot as plt
 import base64
-from django.template.defaulttags import register
 
 def is_valid_queryparam(dictionary, name, param):
     if param != '' and param is not None:
@@ -18,6 +18,14 @@ def is_valid_queryparam(dictionary, name, param):
         dictionary[name] = ''
         return False
     
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return('''{p:.2f}%
+({v:d})'''.format(p=pct,v=val))
+    return my_autopct
+
 # Create your views here.
 def index(request):
     """The home page for Reporting."""
@@ -27,10 +35,13 @@ def index(request):
 def inventory(request):
     """The reporting page"""
     qs = order_form.objects.all().prefetch_related('order_set').prefetch_related('order_set__productID').select_related('dealershipID').select_related('salespersonID').order_by('-id')
-    productcounts = qs.values('order__productID__name').annotate(sum=Sum('order__quantity')).order_by('order__productID__name')
+    
+    #for filter
+    product_list = Product.objects.all().values_list('name', flat=True) 
     qs = qs.annotate(totalqty=Sum('order__quantity'))
     qs = qs.annotate(totalsum=Sum((1 - F('order__discount')) * F('order__quantity') * F('order__price'), output_field=FloatField()))
-    product_list = Product.objects.all().values_list('name', flat=True)
+    qs = qs.annotate(search_name=Concat('salespersonID__first_name', Value(' '), 'salespersonID__last_name'))
+    
     name_contains_query = request.GET.get('name_contains')
     salesperson_contains_query = request.GET.get('salesperson')
     totalMin_query = request.GET.get('totalMin')
@@ -43,57 +54,45 @@ def inventory(request):
     updatedDateMin_query = request.GET.get('updatedDateMin')
     updatedDateMax_query = request.GET.get('updatedDateMax')
     
-    values = {}
+    values = {} #keep filter static
 
     if is_valid_queryparam(values, 'name', name_contains_query):
         qs = qs.filter(dealershipID__dealership__icontains=name_contains_query)
     
     if is_valid_queryparam(values, 'salesperson', salesperson_contains_query):
-        qs = qs.filter(Q(salespersonID__first_name__icontains=salesperson_contains_query)
-                    | Q(salespersonID__last_name__icontains=salesperson_contains_query)
-                    ).distinct()
+        qs = qs.filter(search_name__icontains=salesperson_contains_query)
                     
     if is_valid_queryparam(values, 'totalMin', totalMin_query):
         qs = qs.filter(totalsum__gte=totalMin_query)
-
     if is_valid_queryparam(values, 'totalMax', totalMax_query):
         qs = qs.filter(totalsum__lte=totalMax_query)
-
     if is_valid_queryparam(values, 'qtyMin', qtyMin_query):
         qs = qs.filter(totalqty__gte=qtyMin_query)
-
     if is_valid_queryparam(values, 'qtyMax', qtyMax_query):
         qs = qs.filter(totalqty__lte=qtyMax_query)
-
-    if is_valid_queryparam(values, 'product', product_query) and product_query != "Order includes...":
-        qs = qs.filter(order__productID__name__icontains=product_query)
-
     if is_valid_queryparam(values, 'placedDateMin', placedDateMin_query):
         qs = qs.filter(date__gte=placedDateMin_query)
-
     if is_valid_queryparam(values, 'placedDateMax', placedDateMax_query):
         qs = qs.filter(date__lte=placedDateMax_query)
-
     if is_valid_queryparam(values, 'updatedDateMin', updatedDateMin_query):
         qs = qs.filter(updated__gte=updatedDateMin_query)
-
     if is_valid_queryparam(values, 'updatedDateMax', updatedDateMax_query):
         qs = qs.filter(updated__lte=updatedDateMax_query)
+        
+    if is_valid_queryparam(values, 'product', product_query) and product_query != "Order includes...":
+        qs = qs.filter(id__in=qs.filter(order__productID__name__icontains=product_query))
 
-    # dealercounts = qs.values('dealershipID__dealership').annotate(cnt=Count('id',  distinct=True)).order_by('dealershipID__dealership')
-    # print(dealercounts)
 
-    # x = [entry['dealershipID__dealership'] for entry in dealercounts]
-    # y = [entry['cnt'] for entry in dealercounts]
-
-    # print(productcounts)
+    productcounts = qs.values('order__productID__name').annotate(sum=Sum('order__quantity')).order_by('order__productID__name')
+    summarytable = qs.aggregate(Avg('totalsum'),Max('totalsum'), Min('totalsum'), Count('id'))
+    
+    # make products sold by qty pie chart
     labels = list(filter(None,[entry['order__productID__name'] for entry in productcounts]))
     sums = list(filter(None,[entry['sum'] for entry in productcounts]))
-    print(sums)
+    # print(sums)
     fig, ax = plt.subplots()
-    ax.pie(sums, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax.pie(sums, labels=labels, autopct=make_autopct(sums), pctdistance=0.8, startangle=90)
     ax.axis('equal')
-    # # Optional: chart title and label axes.
     ax.set_title("Products Sold by Quantity", fontsize=24)
 
     # Create a bytes buffer for saving image
@@ -102,10 +101,7 @@ def inventory(request):
     image_base640=base64.b64encode(figbuffer.getvalue())
     product_image = image_base640.decode('utf-8')
     figbuffer.close() 
-
-    summarytable = qs.aggregate(Avg('totalsum'),Max('totalsum'), Min('totalsum'), Count('order',distinct=True))
-    print(summarytable)
-    # print(values)
+    
     context = {
         'inventory_list': qs,
         'filtervalues': values,
